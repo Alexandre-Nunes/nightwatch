394d3d098d0dba9c94e234910261661abcfd7b00
Alexandre Nunes <alexandre.santosnunes@gmail.com>
2019-10-28 10:05:36 -0300

    delet tests

--- tests/login.test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'login com sucesso': function(browser) {
-        browser
-        .url('http://zombie-web:5000/login')
-        .waitForElementVisible('.waves-light', 3000)
-        .end();
-    }
-}
