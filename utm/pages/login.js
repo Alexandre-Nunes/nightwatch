9a9e89518d1f821a21b98b351d6f8a35955f9de9
Alexandre Nunes <alexandre.santosnunes@gmail.com>
2019-10-30 20:38:05 -0300

    pageobjects

---
+++ utm/pages/login.js
@@ -0,0 +1,22 @@
+module.exports = {
+        url: ('http://admin-mail.chaordicsystems.com/'),
+        elements: {
+        nameInput: 'input[name=email]',
+        passInput: 'input[name=password]',
+        loginButton: '.btn-large',  
+        toolsButton: '.tools',
+        rendererButton: '//li//a//href[contains(text(),"Renderer MAX")]',
+        clienteButton: '.chosen-container',
+        clienteInput: 'input[type=text',
+        confirmaCliente: '.active-result',
+        campanhaButton: '#type_chosen',
+        selectCampanha: '//li[contains(text(),"abandoned_cart")]',
+        selectDummy: '#use_dummy_recs',
+        renderButton: '.btn-primary',
+        form: '#theSubject',
+       // url2: 'https://br-store.acer.com/t/20/?utm_source=ShopBack&utm_medium=ShopTarget_abandonodecarrinho&utm_campaign=email-nc',
+       // assertnc: 'email-nc',
+       // screenschot: 'tests_output/client.png'
+
+    }
+}
--- utm/tests/login.js
+++ utm/tests/login.js
@@ -1,48 +1,33 @@
 module.exports = {
-    'login com sucesso': function(browser) {
-        browser
-        
-        ///////// Acessando a url ///////////////
-        .url('http://admin-mail.chaordicsystems.com/')
-        ///////// Realizando o login ////////////
-        .setValue('input[name=email]', 'mail@chaordicsystems.com')
-        .setValue('input[name=password]', 'gK*P-#TqDUSXZsfK*-7J')
-        .click('.btn-large')
-        //////// Acessando o RendererMax ////////
-        .click('.tools')
-        .useXpath()
-        .click('//li//a[contains(text(),"Renderer MAX")]')
-        .useCss()
-        /////// Selecionanco o cliente //////////
-        .click('.chosen-container')
-        .setValue('input[type=text', 'Acer')
-        .click('.active-result')
-        .pause(3000)              
-        //////// selecionando campanha ///////////
-        .click('#type_chosen')
-        .pause(3000)  
-        .useXpath()
-        .click('//li[contains(text(),"abandoned_cart")]')
-        .useCss()
-        .pause(3000)
-        .click('#use_dummy_recs')
-        .pause(3000)
-        .click('.btn-primary')
-        .pause(3000)
-        .waitForElementVisible('#theSubject', 5000)
-               
-        //////// clicando no produto ////////////
-        .url('https://br-store.acer.com/t/20/?utm_source=ShopBack&utm_medium=ShopTarget_abandonodecarrinho&utm_campaign=email-nc')
-                       
-        /////// validando parâmetros da url (nc) ////////
-        .assert.urlContains('email-nc')
-        .saveScreenshot('tests_output/client.png')
-         
-    } 
-        
+    'login com sucesso': function (browser) {
+       
+        var login = browser.page.login()
+
+        login
+            .navigate()
+            .setValue('@nameInput', 'mail@chaordicsystems.com')
+            .setValue('@passInput', 'gK*P-#TqDUSXZsfK*-7J')
+            .click('@loginButton')
+            .click('@toolsButton')
+            .useXpath()
+            .waitForElementVisible('//li//a[contains(text(),"Renderer MAX")]', 5000)
+            .click('//li//a[contains(text(),"Renderer MAX")]')
+            .useCss()
+            .click('@clienteButton')
+            .setValue('@clienteInput', 'Acer')
+            .click('@confirmaCliente')
+            .click('@campanhaButton')
+            .useXpath()
+            .waitForElementVisible('//li[contains(text(),"abandoned_cart")]', 3000)
+            .click('//li[contains(text(),"abandoned_cart")]')
+            .useCss()
+            .click('@selectDummy')
+            .click('@renderButton')
+            .pause(3000)
+            .waitForElementVisible('@form', 3000)
+            //.url('https://br-store.acer.com/t/20/?utm_source=ShopBack&utm_medium=ShopTarget_abandonodecarrinho&utm_campaign=email-nc')
+            //.assert.urlContains('@assertnc')
+            //.saveScreenshot('@screenschot')
+    }
 }
 
-        
-       
-           // .assert.containsText(userInfo, 'Quilombo')
-           
