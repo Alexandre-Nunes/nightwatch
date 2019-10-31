115885d11bd57ffdb4500d7c816b200cc328af55
Alexandre Nunes <alexandre.santosnunes@gmail.com>
2019-10-31 13:38:05 -0300

    ajustes novos

--- utm/hooks/globals.js
+++ utm/hooks/globals.js
@@ -6,17 +6,17 @@ module.exports = {
        browser.resizeWindow(1920, 1080)
 
        login
-       .navigate()
-       .setValue('@nameInput', 'mail@chaordicsystems.com')
-       .setValue('@passInput', 'gK*P-#TqDUSXZsfK*-7J')
-       .click('@loginButton')
-       .click('@toolsButton')
-       .useXpath()
-       .waitForElementVisible('//li//a[contains(text(),"Renderer MAX")]', 5000)
-       .click('//li//a[contains(text(),"Renderer MAX")]')
-       .useCss()
-       .click('@clienteButton')
-        done()
+           .navigate()
+           .setValue('@nameInput', 'mail@chaordicsystems.com')
+           .setValue('@passInput', 'gK*P-#TqDUSXZsfK*-7J')
+           .click('@loginButton')
+           .click('@toolsButton')
+           .useXpath()
+           .waitForElementVisible('//li//a[contains(text(),"Renderer MAX")]', 5000)
+           .click('//li//a[contains(text(),"Renderer MAX")]')
+           .useCss()
+           .click('@clienteButton')
+            done()
      },
  
      afterEach: function (browser, done) {
