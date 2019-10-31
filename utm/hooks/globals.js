ea03c6f440cdeef67b82fa12f4c749127ea28fbb
Alexandre Nunes <alexandre.santosnunes@gmail.com>
2019-10-31 16:18:43 -0300

    novo teste

--- utm/hooks/globals.js
+++ utm/hooks/globals.js
@@ -1,22 +1,10 @@
 module.exports = {
     // '@disabled': true,
      beforeEach: function (browser, done) {
-      var login = browser.page.login()
+      
 
        browser.resizeWindow(1920, 1080)
-
-       login
-           .navigate()
-           .setValue('@nameInput', 'mail@chaordicsystems.com')
-           .setValue('@passInput', 'gK*P-#TqDUSXZsfK*-7J')
-           .click('@loginButton')
-           .click('@toolsButton')
-           .useXpath()
-           .waitForElementVisible('//li//a[contains(text(),"Renderer MAX")]', 5000)
-           .click('//li//a[contains(text(),"Renderer MAX")]')
-           .useCss()
-           .click('@clienteButton')
-            done()
+         done()
      },
  
      afterEach: function (browser, done) {
--- utm/tests/login.js
+++ utm/tests/login.js
@@ -1,9 +1,19 @@
 module.exports = {
-    'login com sucesso': function (browser) {
-       
+    'valdar utm=email-nc na campanha abandoned_cart com sucesso': function (browser) {
+
         var login = browser.page.login()
 
         login
+            .navigate()
+            .setValue('@nameInput', 'mail@chaordicsystems.com')
+            .setValue('@passInput', 'gK*P-#TqDUSXZsfK*-7J')
+            .click('@loginButton')
+            .click('@toolsButton')
+            .useXpath()
+            .waitForElementVisible('//li//a[contains(text(),"Renderer MAX")]', 5000)
+            .click('//li//a[contains(text(),"Renderer MAX")]')
+            .useCss()
+            .click('@clienteButton')
             .setValue('@clienteInput', 'Acer')
             .click('@confirmaCliente')
             .click('@campanhaButton')
@@ -15,11 +25,45 @@ module.exports = {
             .click('@renderButton')
             .pause(5000)
             .waitForElementVisible('@form', 3000)
+            .pause(5000)
+        browser.url('https://br-store.acer.com/t/20/?utm_source=ShopBack&utm_medium=ShopTarget_abandonodecarrinho&utm_campaign=email-nc')
+            .assert.urlContains('email-nc')
+            .saveScreenshot('@screenschot')
+
+
+    },
+
+    'valdar utm=email-nc na campanha abandoned_session com sucesso': function (browser) {
+
+        var login = browser.page.login()
+
+        login
+            .navigate()
+            //.setValue('@nameInput', 'mail@chaordicsystems.com')
+            //.setValue('@passInput', 'gK*P-#TqDUSXZsfK*-7J')
+            //.click('@loginButton')
+            .click('@toolsButton')
+            .useXpath()
+            .waitForElementVisible('//li//a[contains(text(),"Renderer MAX")]', 5000)
+            .click('//li//a[contains(text(),"Renderer MAX")]')
+            .useCss()
+            .click('@clienteButton')
+            .setValue('@clienteInput', 'Acer')
+            .click('@confirmaCliente')
+            .click('@campanhaButton')
+            .useXpath()
+            .waitForElementVisible('//li[contains(text(),"abandoned_session")]', 3000)
+            .click('//li[contains(text(),"abandoned_session")]')
+            .useCss()
+            .click('@selectDummy')
+            .click('@renderButton')
+            .pause(5000)
+            .waitForElementVisible('@form', 3000)
+            .pause(5000)
              browser.url('https://br-store.acer.com/t/20/?utm_source=ShopBack&utm_medium=ShopTarget_abandonodecarrinho&utm_campaign=email-nc')
             .assert.urlContains('email-nc')
             .saveScreenshot('@screenschot')
 
-            
     }
 }
 
